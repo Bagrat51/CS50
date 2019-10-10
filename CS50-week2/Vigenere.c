da60e5c80497360fa17797fd8b46c39a99a75ea3
Bagrat51 <araqelyan.bagrat@yadnex.ru>
2019-10-10 15:25:01 +0400

    debug

--- CS50-week2/Vigenere.c
+++ CS50-week2/Vigenere.c
@@ -2,61 +2,58 @@
 #include <stdio.h>
 #include <string.h>
 
-int shift (char c);
+int shift(char c)
+{
+    if (c <= 122 && c >= 97)
+    {
+        c -= 97;
+    }
+    else if (c >= 64 && c <= 90)
+    {
+        c -= 65;
+    }
+    return c;
+}
 
 int main(int argc, string argv[])
 {
-	//inserting key and text
-    string key;
-    string text = get_string("Insert text: ");
 
+    string key;
     // key = argv [1]
-    if (argc == 2)
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar keyword\n");
+        return 1;
+        exit(0);
+    }
+    else if (argc == 2)
     {
         key = argv[1];
-	}
-    else
-    {
-        printf("Usage: ./vigenere keyword");
     }
-
-    //fuction to each key character
-    int shift (char c)
-    {
-		if (c <= 122 && c > 96)
-	    {
-	        c -= 96;
-	        c %= 26;
-	    }
-	    else if (c >= 65 && c <= 90)
-	    {
-	        c -= 65;
-	        c %= 26;
-	    }
-    }
-
+    string text = get_string("Insert text: "); // how exclude numbers from string ??
     //main crypter
     for (int i = 0; i < text[i] ; i++)
     {
+        int j = i % strlen(key); //      dont repeat on smbols?? 
         if (text[i] <= 122 && text[i] > 96)
         {
             text[i] -= 96;
-            // here key is repeating for every text[i] character
-            text[i] += shift(key[i]);
+            // here key is repeating for every text[i] character               
+            text[i] += shift(key[j]);
             text[i] %= 26;
             text[i] += 96;
         }
-        else if (text[i] >= 65 && text[i] <= 90)
+        else if (text[i] >= 64 && text[i] <= 90)
         {
-            text[i] -= 65;
-            text[i] += shift(key[i]);
+            text[i] -= 64; //check
+            text[i] += shift(key[j]);
             text[i] %= 26;
-            text[i] += 65;
+            text[i] += 64;
         }       
     }
-    //crypted text
-    printf("%s\n", text);
+    printf("ciphertext: %s\n", text);
     
 }
+
 
 
